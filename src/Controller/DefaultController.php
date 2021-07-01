5c4a2255632eae6f204d1859042a5d82daabc341
andsaru <andsaru@hotmail.es>
2021-07-01 13:59:09 +0200

    Añadir el controlador API

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController 
@@ -23,4 +25,16 @@ class DefaultController
         // Symfony\Component\HttpFoundation\Response
         return new Response('hola');
     }
+
+    // Redirige a la URL del back o puede redirigir al front?
+    // public function redirectToHome(): RedirectResponse {
+    //     // Redirigir a la URL /
+    //     // return $this->redirect('/');
+
+    //     // Redirigir a una ruta utilizando su nombre.
+    //     // return $this->redirectToRoute('default_show', ['id' => 1]);
+
+    //     // Devolver directamente un objeto RedirectResponse.
+    //     return new RedirectResponse('/', Response::HTTP_TEMPORARY_REDIRECT);
+    // }
 }
